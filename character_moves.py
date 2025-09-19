af29e43845293d1ba64c49779bd89abb25467c4c
park suhyeok <apato4125@gmail.com>
2025-09-19 23:32:08 +0900

    함수 호출

--- character_moves.py
+++ character_moves.py
@@ -2,7 +2,19 @@ from pico2d import *
 
 open_canvas()
 
+def rectangle_move():
+    pass
+
+def triangle_move():
+    pass
+
+def circle_move():
+    pass
+
 while True:
+    rectangle_move()
+    triangle_move()
+    circle_move()
     pass
 
 close_canvas()
