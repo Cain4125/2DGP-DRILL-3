f0201f0b522d98f41df3d8358636b0da98fe73ef
park suhyeok <apato4125@gmail.com>
2025-09-19 23:42:19 +0900

    사각형 이동 함수 작성

--- character_moves.py
+++ character_moves.py
@@ -1,9 +1,47 @@
 from pico2d import *
 
 open_canvas()
-
+grass = load_image('grass.png')
+character = load_image('character.png')
+x=400
+y=90
 def rectangle_move():
-    pass
+    global x,y
+    while x < 785:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.draw(x,y)
+        update_canvas()
+        x = x + 2
+        delay(0.001)
+    while y<560:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.draw(x,y)
+        update_canvas()
+        y = y + 2
+        delay(0.001)
+    while x > 15:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.draw(x,y)
+        update_canvas()
+        x = x - 2
+        delay(0.001)
+    while y>90:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.draw(x,y)
+        update_canvas()
+        y = y - 2
+        delay(0.001)
+    while x < 400:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.draw(x,y)
+        update_canvas()
+        x = x + 2
+        delay(0.001) #(800,310)
 
 def triangle_move():
     pass
